4e6b5af4fa83faefe59a8e3127ef008bb261bda1
lucas pereira <lucas.pereira@canopusonline.com>
2019-05-29 01:14:16 -0300

    [WIP] Alteracoes parciais nas rotas e no front end.

--- app/auth.py
+++ app/auth.py
@@ -7,11 +7,29 @@ bp = Blueprint('auth', __name__)
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
+    try:
+        if request.method == 'POST':
+            _user = request.form['email']
+            _password = request.form['senha']
+            if _user == 'lucas@teste.com' and _password == 'a1b2c3':
+                _login = True
 
-    return render_template('auth/register.html')
+        if _login:
+            return redirect(url_for('taskboard.home'))
+    except:
+        return render_template('auth/register.html')
 
 
 @bp.route('/', methods=('GET', 'POST'))
 def login():
+    try:
+        if request.method == 'POST':
+            _user = request.form['email']
+            _password = request.form['senha']
+            if _user == 'lucas@teste.com' and _password == 'a1b2c3':
+                _login = True
 
-    return render_template('auth/login.html')
+        if _login:
+            return redirect(url_for('taskboard.home'))
+    except:
+        return render_template('auth/login.html')
