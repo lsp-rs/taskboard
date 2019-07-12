d23c97c350c2cd74c0b5e4fd430729cfc363e55c
lucas pereira <lucas.pereira@canopusonline.com>
2019-07-12 09:18:18 -0300

    [WIP] Card controller.

---
+++ app/controllers/CardController.py
@@ -0,0 +1,23 @@
+from flask import session, redirect, url_for
+from app.helpers.CardHelperDB import CardDao
+from datetime import datetime
+
+class CardController():
+
+    _crd = CardDao()
+    def insertCard(self, data):
+        try:
+            if 'title_card' in data:
+                self._crd.insertCard(data)
+        except Exception as e:
+            print(e)
+            return False
+        return True
+
+    def updateCard(self, data):
+        try:
+            if 'title_card' in data:
+                self._crd.updateCard(data)
+        except Exception as e:
+            print(e)
+        return True
